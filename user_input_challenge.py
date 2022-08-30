b5b2c7a1d99ae857a7cdb617a1455c697b6f7550
andyarmor <aoarmor25@students.bmchs.org>
2022-08-30 18:20:12 -0500

    took longer than should have

--- user_input_challenge.py
+++ user_input_challenge.py
@@ -11,3 +11,8 @@ many copies of the previous message. (Hint: order of operations exists in Python
 Print out that many copies of the previous message on separate lines. 
 (Hint: the string "\n" is the same as pressing the ENTER button)
 '''
+# year you are 100 
+print("We are going to see what year you will turn 100 years old!")
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+print("Year when you will turn 100 is:", 100 - age + 2022)
